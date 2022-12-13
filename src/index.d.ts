8282c1c98a77c4f469fe2e1fd85c919dc6b0dc37
Julie Han <juliemenghan@gmail.com>
2022-12-13 14:42:54 -0500

    property data type

--- src/index.d.ts
+++ src/index.d.ts
@@ -18,4 +18,34 @@ type MessageData = {
   readAt: string;
 };
 
-export { UserData, MessageData };
+type PropertyData = {
+  id: number;
+  title: string;
+  street: string;
+  city: string;
+  state: string;
+  zipcode: string;
+  latitude: string;
+  longitude: string;
+  description: string;
+  price: number;
+  username: string;
+};
+
+export { UserData, MessageData, PropertyData };
+
+/**
+ * CREATE TABLE properties (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  street VARCHAR(255) NOT NULL,
+  city VARCHAR(30) NOT NULL,
+  state VARCHAR(30) NOT NULL,
+  zipcode VARCHAR(11) NOT NULL,
+  latitude VARCHAR(14) NOT NULL,
+  longitude VARCHAR(14) NOT NULL,
+  description TEXT NOT NULL,
+  price INTEGER NOT NULL,
+  owner_username VARCHAR(25) NOT NULL REFERENCES users ON DELETE CASCADE
+);
+ */
