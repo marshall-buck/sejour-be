b72d795523ad41b2995fa6085a2f13ad3db1e707
Julie Han <juliemenghan@gmail.com>
2022-12-13 14:27:46 -0500

    formatting

--- sejour-schema.sql
+++ sejour-schema.sql
@@ -3,14 +3,13 @@
 -- DROP TABLE IF EXISTS bookings;
 -- DROP TABLE IF EXISTS properties;
 -- DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
   username VARCHAR(25) PRIMARY KEY,
   password TEXT NOT NULL,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   email TEXT NOT NULL,
-    CHECK (position('@' IN email) > 1),
+  CHECK (position('@' IN email) > 1),
   is_admin BOOLEAN NOT NULL DEFAULT FALSE,
   avatar TEXT NOT NULL DEFAULT 'https://picsum.photos/100'
 );
@@ -26,18 +25,15 @@ CREATE TABLE properties (
   longitude VARCHAR(14) NOT NULL,
   description TEXT NOT NULL,
   price INTEGER NOT NULL,
-  owner_username VARCHAR(25) NOT NULL
-        REFERENCES users ON DELETE CASCADE
+  owner_username VARCHAR(25) NOT NULL REFERENCES users ON DELETE CASCADE
 );
 
 CREATE TABLE bookings (
   id SERIAL PRIMARY KEY,
   start_date DATE NOT NULL,
   end_date DATE NOT NULL,
-  property_id INT NOT NULL
-        REFERENCES properties ON DELETE CASCADE,
-  guest_username VARCHAR(25) NOT NULL
-      REFERENCES users ON DELETE CASCADE
+  property_id INT NOT NULL REFERENCES properties ON DELETE CASCADE,
+  guest_username VARCHAR(25) NOT NULL REFERENCES users ON DELETE CASCADE
 );
 
 CREATE TABLE messages (
@@ -47,11 +43,10 @@ CREATE TABLE messages (
   body TEXT NOT NULL,
   sent_at TIMESTAMP WITH TIME ZONE NOT NULL,
   read_at TIMESTAMP WITH TIME ZONE
-  );
+);
 
 CREATE TABLE images (
   id SERIAL PRIMARY KEY,
-  key VARCHAR(255) NOT NULL ,
-  property_id INT NOT NULL
-    REFERENCES properties ON DELETE CASCADE
+  key VARCHAR(255) NOT NULL,
+  property_id INT NOT NULL REFERENCES properties ON DELETE CASCADE
 );
