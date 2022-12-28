9db8fb2db0636d8945dea7eb23eb8b85774b43fd
Marshall Buck <dev@marshallbuck.com>
2022-12-28 11:06:11 -0600

    added delete image tests

--- src/models/imageModel.test.ts
+++ src/models/imageModel.test.ts
@@ -12,6 +12,7 @@ import {
   commonAfterEach,
   commonAfterAll,
   propertyIds,
+  imageIds,
 } from "./_testCommon";
 
 beforeAll(commonBeforeAll);
@@ -40,6 +41,43 @@ describe("getAllByProperty", function () {
   test("throws NotFoundError when no property exists", async function () {
     try {
       await Image.getAllByProperty(0);
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+      const errStatus = (err as NotFoundError).status;
+      expect(errStatus).toEqual(404);
+    }
+  });
+});
+describe("create image", function () {
+  test("adds image to db", async function () {
+    const images = await Image.create({
+      imageKey: "test 1",
+      propertyId: propertyIds[0],
+    });
+
+    expect(images).toEqual({
+      id: expect.any(Number),
+      imageKey: "test 1",
+      propertyId: propertyIds[0],
+      isCoverImage: false,
+    });
+  });
+});
+
+describe("delete image", function () {
+  test("deletes image from db", async function () {
+    await Image.delete(imageIds[0]);
+    const images = await db.query(`SELECT id
+                              FROM images
+                                WHERE id = ${imageIds[0]}`);
+
+    expect(images.rows).toEqual([]);
+  });
+
+  test("throws not found if no such image", async function () {
+    try {
+      await Image.delete(0);
       throw new Error("fail test, you shouldn't get here");
     } catch (err) {
       expect(err instanceof NotFoundError).toBeTruthy();
