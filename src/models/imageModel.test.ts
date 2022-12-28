168995f409169529f41de606b2c878289e60f37e
Marshall Buck <dev@marshallbuck.com>
2022-12-28 10:12:20 -0600

    changed image test file to ts

---
+++ src/models/imageModel.test.ts
@@ -0,0 +1,50 @@
+import {
+  NotFoundError,
+  BadRequestError,
+  UnauthorizedError,
+} from "../expressError";
+import { Image } from "./imageModel";
+import { db } from "../db";
+
+import {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+  propertyIds,
+} from "./_testCommon";
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** getAllByProperty */
+
+describe("getAllByProperty", function () {
+  test("returns results", async function () {
+    const images = await Image.getAllByProperty(propertyIds[0]);
+
+    expect(images).toEqual([
+      { id: expect.any(Number), imageKey: "12345678", isCoverImage: true },
+      { id: expect.any(Number), imageKey: "23456789", isCoverImage: false },
+      { id: expect.any(Number), imageKey: "34567890", isCoverImage: false },
+    ]);
+  });
+
+  test("returns no images", async function () {
+    const images = await Image.getAllByProperty(propertyIds[1]);
+    expect(images).toEqual([]);
+  });
+
+  test("throws NotFoundError when no property exists", async function () {
+    try {
+      await Image.getAllByProperty(0);
+      throw new Error("fail test, you shouldn't get here");
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+      const errStatus = (err as BadRequestError).status;
+      expect(errStatus).toEqual(400);
+    }
+  });
+});
