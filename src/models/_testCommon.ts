1c10101ac1fc8b91be2f9de80ae54fec7633faec
Julie Han <juliemenghan@gmail.com>
2022-12-29 16:50:19 -0500

    refactor test before all to separate fn()

--- src/models/_testCommon.ts
+++ src/models/_testCommon.ts
@@ -15,82 +15,11 @@ async function commonBeforeAll() {
   await db.query("DELETE FROM properties");
   await db.query("DELETE FROM users");
 
-  await db.query(
-    ` INSERT INTO users (username,
-                        password,
-                        first_name,
-                        last_name,
-                        email,
-                        avatar)
-      VALUES ('u1', $1, 'U1F', 'U1L','u1@email.com',  'test url'),
-             ('u2', $2, 'U2F', 'U2L','u2@email.com',  'test url')
-      RETURNING username
-    `,
-    [
-      await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
-      await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
-    ]
-  );
-  const resultsProperties = await db.query(`
-      INSERT INTO properties (title,
-                              street,
-                              city,
-                              state,
-                              zipcode,
-                              latitude,
-                              longitude,
-                              description,
-                              price,
-                              owner_username,
-                              archived)
-      VALUES ('property one', '123 lane', 'test city', 'test state', '11111',
-      '180.0000000', '-180.0000000', 'test description', 100, 'u1', false),
-             ('property two', '123 lane', 'test city', 'test state', '11111',
-      '180.0000000', '-180.0000000', 'test description pool', 200, 'u2', false),
-             ('property three', '123 lane', 'test city', 'test state', '11111',
-      '180.0000000', '-180.0000000', 'test description', 200, 'u2', true)
-      RETURNING id
-    `);
-
-  propertyIds.splice(
-    0,
-    0,
-    ...resultsProperties.rows.map((r: PropertyData) => r.id)
-  );
-
-  const resultsMessages = await db.query(`
-      INSERT INTO messages (from_username, to_username, body, sent_at)
-          VALUES ('u1', 'u2', 'test message', current_timestamp)
-              RETURNING id`);
-
-  messageIds.splice(
-    0,
-    0,
-    ...resultsMessages.rows.map((r: MessageData) => r.id)
-  );
-
-  const resultsImages = await db.query(`
-      INSERT INTO images (image_key, property_id, is_cover_image)
-          VALUES ('12345678', ${propertyIds[0]}, true),
-                 ('23456789', ${propertyIds[0]}, false),
-                 ('34567890', ${propertyIds[0]}, false)
-                RETURNING id
-      `);
-
-  imageIds.splice(0, 0, ...resultsImages.rows.map((r: ImageData) => r.id));
-
-  const resultsBookings = await db.query(`
-        INSERT INTO bookings (start_date, end_date, property_id, guest_username)
-            VALUES('2022-11-29T05:00:00.000Z',
-                  '2022-11-30T05:00:00.000Z',
-                  ${propertyIds[0]}, 'u2')
-                  RETURNING id
-      `);
-  bookingIds.splice(
-    0,
-    0,
-    ...resultsBookings.rows.map((r: BookingData) => r.id)
-  );
+  await seedUsers();
+  await seedProperties();
+  await seedMessages();
+  await seedImages();
+  await seedBookings();
 }
 
 async function commonBeforeEach() {
@@ -115,3 +44,98 @@ export {
   imageIds,
   bookingIds,
 };
+
+/******************** HELPER FUNCTIONS FOR SEEDING DB BEFORE ALL **************/
+
+/** Seed users DB with test users and encrypts passwords before insertion */
+async function seedUsers() {
+  await db.query(
+    ` INSERT INTO users (username,
+                        password,
+                        first_name,
+                        last_name,
+                        email,
+                        avatar)
+      VALUES ('u1', $1, 'U1F', 'U1L','u1@email.com',  'test url'),
+             ('u2', $2, 'U2F', 'U2L','u2@email.com',  'test url')
+      RETURNING username
+    `,
+    [
+      await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
+      await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
+    ]
+  );
+}
+
+/** Seed properties DB with test properties, creates an array of property Ids */
+async function seedProperties() {
+  const resultsProperties = await db.query(`
+  INSERT INTO properties (title,
+                          street,
+                          city,
+                          state,
+                          zipcode,
+                          latitude,
+                          longitude,
+                          description,
+                          price,
+                          owner_username,
+                          archived)
+  VALUES ('property one', '123 lane', 'test city', 'test state', '11111',
+  '180.0000000', '-180.0000000', 'test description', 100, 'u1', false),
+         ('property two', '123 lane', 'test city', 'test state', '11111',
+  '180.0000000', '-180.0000000', 'test description pool', 200, 'u2', false),
+         ('property three', '123 lane', 'test city', 'test state', '11111',
+  '180.0000000', '-180.0000000', 'test description', 200, 'u2', true)
+  RETURNING id
+`);
+
+  propertyIds.splice(
+    0,
+    0,
+    ...resultsProperties.rows.map((r: PropertyData) => r.id)
+  );
+}
+
+/** Seed messages DB with test messages, creates an array of message Ids */
+async function seedMessages() {
+  const resultsMessages = await db.query(`
+      INSERT INTO messages (from_username, to_username, body, sent_at)
+          VALUES ('u1', 'u2', 'test message', current_timestamp)
+              RETURNING id`);
+
+  messageIds.splice(
+    0,
+    0,
+    ...resultsMessages.rows.map((r: MessageData) => r.id)
+  );
+}
+
+/** Seed images DB with test images, creates an array of image Ids */
+async function seedImages() {
+  const resultsImages = await db.query(`
+      INSERT INTO images (image_key, property_id, is_cover_image)
+          VALUES ('12345678', ${propertyIds[0]}, true),
+                 ('23456789', ${propertyIds[0]}, false),
+                 ('34567890', ${propertyIds[0]}, false)
+                RETURNING id
+      `);
+
+  imageIds.splice(0, 0, ...resultsImages.rows.map((r: ImageData) => r.id));
+}
+
+/** Seed bookings DB with test bookings, creates an array of booking Ids */
+async function seedBookings() {
+  const resultsBookings = await db.query(`
+  INSERT INTO bookings (start_date, end_date, property_id, guest_username)
+      VALUES('2022-11-29T05:00:00.000Z',
+            '2022-11-30T05:00:00.000Z',
+            ${propertyIds[0]}, 'u2')
+            RETURNING id
+`);
+  bookingIds.splice(
+    0,
+    0,
+    ...resultsBookings.rows.map((r: BookingData) => r.id)
+  );
+}
