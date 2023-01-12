ba1945650b7a26d8a48a4e353a2980c7e0b22574
Julie Han <juliemenghan@gmail.com>
2023-01-12 13:28:35 -0500

    added coverage for err.stack

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import app from "./app";
 import { db } from "./db";
+import { testUsers } from "./routes/_testCommon";
 
 test("not found for site 404", async function () {
   const res = await request(app).get("/no-such-path");
@@ -8,9 +9,18 @@ test("not found for site 404", async function () {
 });
 
 test("not found for site 404 (test stack print)", async function () {
+  process.env.NODE_ENV = "";
   const res = await request(app).get("/no-such-path");
   expect(res.statusCode).toEqual(404);
+  process.env.END
 });
+
+// test("500 server error", async function () {
+//   const res = await request(app)
+//     .get("/message/$0")
+//     .set("authorization", `Bearer ${testUsers[0].token}`);
+//   expect(res.statusCode).toEqual(500);
+// });
 
 afterAll(function () {
   db.end();
